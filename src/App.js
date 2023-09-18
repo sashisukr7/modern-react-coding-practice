e3b83a30985370683f4663ad85e933c72ed73e6b
Sashisu Kumar <sashisu_kumar@trisysit.com>
2023-09-18 17:10:28 +0530

    choose

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+//  git option after creating project in github, choose below option
+//  …or push an existing repository from the command line
+//git remote add origin https://github.com/sashisukr7/modern-react-coding-practice.git
+//git branch -M main
+//git push -u origin main
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload sss.
         </p>
         <a
           className="App-link"
